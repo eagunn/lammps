9508408be3f101048de88c114a38c1bf74cbea04
Anne Gunn <agunn@sherprog.com>
2020-07-20 05:49:41 -0600

    WIP -- Address next set of valgrind issues. 1) I've verified, by pulling, building, and testing old versions, that since at least July 2019, valgrind's been reporting an invalid read of a[n], one place outside the allocated memory for a. Reading the code, I concur. This may explain why once in a long while I get error messages from the compute_pressure_bocs code on one run but not on the next with the same input. So, I've change the calloc for a to n+1, like the other arrays, and initialized a[n] to 0.0, as is done for the other arrays. 2) I've added a coding trick, in the FixBocs destructor, to eliminate a huge number of "still reachable" warnings in valgrind output. This could be eliminated if we think it's inefficient. But, for now, I'm prioritizing a cleaner valgrind report.

--- src/USER-BOCS/compute_pressure_bocs.cpp
+++ src/USER-BOCS/compute_pressure_bocs.cpp
@@ -123,27 +123,24 @@ ComputePressureBocs::~ComputePressureBocs()
   delete [] vptr;
   if (phi_coeff) free(phi_coeff);
 
-  // Linear splines data that was passed in from fix_bocs must be
+  // Any splines data that was sent in from fix_bocs must be
   // freed here, not in fix_bocs where it was calloc'd
-
-  // If it was used, we must free memory in spline array before we terminate
   if (splines) {
-    error->all(FLERR,"in ComputePressureBocs destructor, about to free splines data\n");
     if (p_basis_type == BASIS_LINEAR_SPLINE) {
-      // splines is a 2 by numSplines matrix
+      // splines is a 2 by numSplines matrix of doubles
       for (int i = 0; i < NUM_LINEAR_SPLINE_COLUMNS; ++i) {
         free(splines[i]);
         splines[i] = NULL;
       }
     }
     else if (p_basis_type == BASIS_CUBIC_SPLINE) {
-      // splines is a 5 by numSplines matrix
+      // splines is a 5 by numSplines matrix of doubles
       for (int i = 0; i < NUM_CUBIC_SPLINE_COLUMNS; ++i) {
         free(splines[i]);
         splines[i] = NULL;
       }
     }
-    // else splines not an issue for analytic basis
+    // no else clause intentional; splines not an issue for analytic basis
     free(splines);
     splines = NULL;
   }
